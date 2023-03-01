a86d836199455651f126dd7f1a563783f35ca42b
Rudraa <106871287+Rudraa2213@users.noreply.github.com>
2023-03-01 13:28:11 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 API_ID = int(os.environ.get("API_ID", "9126459"))
 API_HASH = os.environ.get("API_HASH", "238c912d48a9ec0d0e8b05738f358ffc")
-BOT_TOKEN = os.environ.get("BOT_TOKEN","5772657254:AAF2FlImkbFvlIsEVQr4QE_3go3Bi-cKWpk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","6237349575:AAF4J6Y_6Zk-qPf7rpvRBZmPUt0QB5zCPXw")
 SESSION = str(os.environ.get("SESSION",'BQC9PFsX1DZk77OV0gAZp-xpOJR9HFiwDuRIpJ74USu7u9z30f-iBoi_h3KWEYNXs7PGB86l9vWkW32u6JWsPxtzafxtx9v4On2dk2_is7gnDM2rLr2wQ-G36WNKJST1Tap7pbWc7ttdavCgIzQeIVX__SwVFitwolewfNDWt5UrRAXbCeTuS4tYBbqEdEEN2D27OY9uvy8cmnlDbhLwkp15C-gVKKnNXZUiZhYz5Y-fURM4pz-Zx-yrk14nudCoIZTyqDyU1jw7ath2zLVNe2H_-aNLX6YOvavGm2j_OXp2SkfyFEnqlOSQhKkrgE5qVvyGUIaZ_l96BE6EO37nu3WTLH5jRAA'))
 AUTH = int(os.environ.get("BOT_OWNER", "746480452"))
 FORCESUB = config("FORCESUB",'Joint0T')
